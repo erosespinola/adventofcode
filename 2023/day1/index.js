f0e8e8d755b10d99b03b94177b8230f924603f53
Eros Espínola <eros.espinola.gonzalez@gmail.com>
2023-12-06 20:00:01 -0500

    add day 1 readme

--- 2023/day1/index.js
+++ 2023/day1/index.js
@@ -1,42 +1,43 @@
 const fs = require('node:fs')
+const data = fs.readFileSync('data.txt', 'utf8').split('\n')
 
-try {
-  const data = fs.readFileSync('data.txt', 'utf8').split('\n')
-  const firstSum = data.reduce((totalSum, line) => {
-    const match = line.match(/\d/g)
-    let lineSum = 0
-    if (match) {
-      lineSum = parseInt(match.at(0) + match.at(-1))
-    }
-    return totalSum + lineSum
-  }, 0)
-  // 52974
+// Consider your entire calibration document.
+// What is the sum of all of the calibration values?
 
-  const numbers = {
-    one: '1',
-    two: '2',
-    three: '3',
-    four: '4',
-    five: '5',
-    six: '6',
-    seven: '7',
-    eight: '8',
-    nine: '9',
+const calibrationSum = data.reduce((totalSum, line) => {
+  const match = line.match(/\d/g)
+  let lineSum = 0
+  if (match) {
+    lineSum = parseInt(match.at(0) + match.at(-1))
   }
+  return totalSum + lineSum
+}, 0)
 
-  const secondSum = data.reduce((totalSum, line) => {
-    const match = Array.from(
-      line.matchAll(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g)
-    )
+console.log(calibrationSum) // 52974
 
-    const firstDigit = numbers[match.at(0)[1]] || match.at(0)[1]
-    const secondDigit = numbers[match.at(-1)[1]] || match.at(-1)[1]
-    const lineSum = parseInt(firstDigit + secondDigit)
-    return totalSum + lineSum
-  }, 0)
-  // 53340
+// What is the sum of all of the calibration values? (with written numbers)
 
-  console.log(firstSum, secondSum)
-} catch (err) {
-  console.error(err)
+const numbers = {
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
 }
+
+const secondCalibrationSum = data.reduce((totalSum, line) => {
+  const match = Array.from(
+    line.matchAll(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g)
+  )
+
+  const firstDigit = numbers[match.at(0)[1]] || match.at(0)[1]
+  const secondDigit = numbers[match.at(-1)[1]] || match.at(-1)[1]
+  const lineSum = parseInt(firstDigit + secondDigit)
+  return totalSum + lineSum
+}, 0)
+
+console.log(secondCalibrationSum) // 53340
